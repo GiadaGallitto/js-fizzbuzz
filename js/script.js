bcc32d6fa7a51f28e75fe0b7e1760ed0fee27b80
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-23 16:01:30 +0100

    Fix For cicle and Add fizz and buzz Elements

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const fizzElement = "Fizz";
 
+const buzzElement = "Buzz";
+
+const fizzBuzzElement = "FizzBuzz"
 
 for (let index = 1; index <= 100; index++ ) {
-    console.log(index);
+    let element = index;
 
-    if(index % 3 === 0){
-        console.log(fizzElement);
+    if(index % 15 === 0){
+        element = fizzBuzzElement;
+
+    } else if(index % 5 === 0){
+        element = buzzElement;
+
+    } else if(index % 3 === 0){
+        element = fizzElement;
     }
+
+    console.log(element)
 }
