4b5a9b85b026cd9c58b5ccd648b83d9c5e2e6ac6
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-23 15:37:58 +0100

    Create if on for cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+const fizzElement = "Fizz";
+
 
 for (let index = 1; index <= 100; index++ ) {
-    console.log(index)
+    console.log(index);
+
+    if(index % 3 === 0){
+        console.log(fizzElement);
+    }
 }
