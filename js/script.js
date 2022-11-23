aa9d79cfa9ba6d16a9a4081af4656840559276d6
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-23 16:29:19 +0100

    Add append element

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ const fizzElement = "Fizz";
 
 const buzzElement = "Buzz";
 
-const fizzBuzzElement = "FizzBuzz"
+const fizzBuzzElement = "FizzBuzz";
+
+const containerElement = document.getElementById("container");
 
 for (let index = 1; index <= 100; index++ ) {
     let element = index;
+
+    containerElement.append(
+        <div class="box">
+            <h3>
+                ${index}
+            </h3>
+        </div>);
 
     if(index % 15 === 0){
         element = fizzBuzzElement;
