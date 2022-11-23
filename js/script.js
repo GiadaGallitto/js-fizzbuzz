6a89de5b5f923edc8727401a72a71fb2d41426e0
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-23 17:31:15 +0100

    Add div elements and custom colors of div

--- js/script.js
+++ js/script.js
@@ -9,19 +9,25 @@ const containerElement = document.getElementById("container");
 for (let index = 1; index <= 100; index++ ) {
     let element = index;
 
+    const divElement = document.createElement("div");
     
     if(index % 15 === 0){
         element = fizzBuzzElement;
-        const divElement = document.createElement("div");
+        divElement.classList.add("box", "green-fb");
         divElement.append(fizzBuzzElement);
+
     } else if(index % 5 === 0){
         element = buzzElement;
-        
+        divElement.classList.add("box", "blue-buzz");
+        divElement.append(buzzElement);
+
     } else if(index % 3 === 0){
         element = fizzElement;
+        divElement.classList.add("box", "red-fizz");
+        divElement.append(fizzElement);
     }
     
-    containerElement.append(element);
+    containerElement.append(divElement);
 
-    console.log(element)
+    console.log(element);
 }
