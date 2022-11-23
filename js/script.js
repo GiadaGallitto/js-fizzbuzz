b97e542cd102e8d989b24913ff5a0de6a0691a7b
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-23 16:33:18 +0100

    Fix append content

--- js/script.js
+++ js/script.js
@@ -9,22 +9,18 @@ const containerElement = document.getElementById("container");
 for (let index = 1; index <= 100; index++ ) {
     let element = index;
 
-    containerElement.append(
-        <div class="box">
-            <h3>
-                ${index}
-            </h3>
-        </div>);
-
+    
     if(index % 15 === 0){
         element = fizzBuzzElement;
-
+        
     } else if(index % 5 === 0){
         element = buzzElement;
-
+        
     } else if(index % 3 === 0){
         element = fizzElement;
     }
-
+    
+    containerElement.append(element);
+    
     console.log(element)
 }
